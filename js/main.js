72950eabc25fd6a2378e688c2b2e0b2865012831
Mohit Patel <3443mohit@gmail.com>
2020-04-07 22:11:08 -0700

    add services

--- js/main.js
+++ js/main.js
@@ -113,14 +113,14 @@ window.addEventListener("scroll", event => {
   document.querySelector(".display-progress").style.width = progress + "%";
 });
 // Disbaled Right Click
-window.oncontextmenu = function () {
-  return false;
-}
-$(document).keydown(function (event) {
-  if (event.keyCode == 123) {
-    return false;
-  }
-  else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
-    return false;
-  }
-});
+// window.oncontextmenu = function () {
+//   return false;
+// }
+// $(document).keydown(function (event) {
+//   if (event.keyCode == 123) {
+//     return false;
+//   }
+//   else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+//     return false;
+//   }
+// });
